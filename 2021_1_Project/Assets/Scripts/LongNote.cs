0ac73637d58243fe97d6d03a208fb45592ec7e1a
DOKWANMOGI\DO <dogoo3@naver.com>
2021-03-07 03:52:35 +0900

    롱노트 표현, 시작노트 이동 구현

--- 2021_1_Project/Assets/Scripts/LongNote.cs
+++ 2021_1_Project/Assets/Scripts/LongNote.cs
@@ -16,18 +16,22 @@ public class LongNote : MonoBehaviour, IPointerDownHandler, IPointerExitHandler
 {
     public Text _judgeText; // Temp Component
 
-    private Image _departcircle, _arrivecircle, _line;
+    private Image _departcircle, _line;
 
     private Vector3 _arrow, _setarrow, _pos, _setpos; // departCircle 진행방향, departCircle 시작 좌표값
     
     private Color _noteColor;
 
     private bool _isHit, _isSpread; // 노트 터치 여부, 노트 펼쳐짐 여부, 
+    private int _stopindex; // _stopOver 배열 인덱스
+    private float _judgeValue;
 
-    private float _fillCalc;
-
-    [Header("출발지(First), 경유지, 목적지(Last)")]
-    [SerializeField] private Transform[] _point = default;
+    [Header("경유노트")]
+    [SerializeField] private LongNoteStopover[] _stopOver = default;
+    [Header("경유노트의 방향전환 포인트")]
+    [SerializeField] private Transform[] _stopOverPoint = default;
+    [Header("도착 노트")]
+    [SerializeField] private Transform _arriveCircle = default;
     [Header("판정선 축소 속도, 노트 이동속도")]
     [SerializeField] private float _reduceValue = default;
     [Tooltip("노트 이동속도와 라인이 생성되는 속도는 똑같음.")]
@@ -35,34 +39,38 @@ public class LongNote : MonoBehaviour, IPointerDownHandler, IPointerExitHandler
     [Header("판정 범위")]
     [SerializeField] private float _awesomeRange = default, _goodRange = default, _failRange = default, _missRange = default;
 
-    int i = 0;
+    float r;
     private void Awake()
     {
         _departcircle = GetComponent<Image>();
-        _arrivecircle = transform.parent.GetComponent<Image>(); // 도착 노트 이미지
         _line = transform.GetChild(0).GetComponent<Image>(); // 판정선 이미지
     }
     private void Start()
     {
         _pos = _setpos = transform.position; // 초기 출발노트 좌표값 설정
         _isHit = false;
-        _fillCalc = 1 / _arrivecircle.rectTransform.sizeDelta.x;
     }
     private void OnEnable()
     {
         InvokeRepeating("BrightenNote", 0f, 0.05f);
 
         _judgeText.text = ""; // Temp
-
-        _arrow = _setarrow = (_point[1].position - _point[0].position).normalized; // 첫 진행 방향 설정
+        
         _noteColor = Color.gray;
         _noteColor.a = 0;
         _isSpread = true;
+
+        _stopOver[0].SpreadNote(_movedepartcircle);
     }
     private void OnDisable()
     {
-        _arrivecircle.fillOrigin = 1; // Right
-        _arrivecircle.fillAmount = 0;
+    }
+
+    private void Hit(string _message)
+    {
+        _isHit = true;
+        _judgeText.text = _message;
+        _arrow = _setarrow = _stopOverPoint[0].transform.position - transform.position; // 첫 경유지 방향벡터 설정
     }
 
     private void BrightenNote()
@@ -86,41 +94,27 @@ public class LongNote : MonoBehaviour, IPointerDownHandler, IPointerExitHandler
     {
         if (_isHit) // 출발 노트가 움직이는 부분
         {
-            if (i < _point.Length - 1)
+            if(_stopindex < _stopOver.Length)
             {
-                if (Vector3.Distance(transform.position, _point[i + 1].position) <= 5.0f)
-                {
-                    i++;
-                    if (i < _point.Length - 1)
-                        _arrow = _setarrow = (_point[i + 1].position - _point[i].position).normalized; // 새로운 진행 방향 설정
-                }
                 _arrow *= _movedepartcircle * Time.deltaTime;
                 transform.position += _arrow;
-                _arrivecircle.fillAmount = Mathf.Abs(transform.position.x - _arrivecircle.transform.position.x) * _fillCalc;
+                _stopOver[_stopindex].SetFillAmount(transform.position);
                 _arrow = _setarrow;
+                if (Vector3.Distance(transform.position, _stopOverPoint[_stopindex].position) < 5.0f)
+                {
+                    _stopOver[_stopindex].SetFillAmount(0);
+                    _stopindex++;
+                    if (_stopindex < _stopOver.Length)
+                        _arrow = _setarrow = (_stopOver[_stopindex].transform.position - transform.position).normalized; // 경유지 방향벡터 설정
+                }
             }
 
-            if (Vector3.Distance(transform.position, _point[_point.Length-1].position) <= 5.0f) // 100.0f는 노트의 반지름 상수값.
+            if(Vector3.Distance(transform.position,_stopOverPoint[_stopOver.Length-1].position) <= 5.0f)
             {
-                i = 0;
-                // OnPointerDown에서 잡은 판정선 따라 판정처리
-                transform.position = _pos = _setpos;
-                _arrow = _setarrow = (_point[1].position - _point[0].position).normalized; // 새로운 진행 방향 설정
-            }
-        }
-        else if (_isSpread) // 노트가 생성되는 부분
-        {
-            _arrow *= _movedepartcircle * Time.deltaTime;
-            _pos += _arrow;
-            _arrivecircle.fillAmount += _arrow.magnitude * _fillCalc;
-            _arrow = _setarrow;
-
-            if (_arrivecircle.fillAmount == 1.0f)
-            {
-                _arrivecircle.fillOrigin = 0; // LEFT
-                i = 0;
-                _isSpread = false;
-                _arrow = _setarrow = (_point[1].position - _point[0].position).normalized; // 새로운 진행 방향 설정
+                transform.position = _setpos;
+                _stopOver[0].SpreadNote(_movedepartcircle);
+                for (int i = 0; i < _stopOver.Length; i++)
+                    _stopOver[i].ResetFillOrigin();
             }
         }
     }
@@ -128,6 +122,20 @@ public class LongNote : MonoBehaviour, IPointerDownHandler, IPointerExitHandler
     public void OnPointerDown(PointerEventData eventData)
     {
         _isHit = true;
+        _arrow = _setarrow = (_stopOver[0].transform.position - transform.position).normalized; // 첫 경유지 방향벡터 설정
+        _stopindex = 0;
+        //if(!_isHit)
+        //{
+        //    _judgeValue = _line.rectTransform.sizeDelta.x - _departcircle.rectTransform.sizeDelta.x;
+        //    // 판정라인 설정
+        //    if (_judgeValue < _awesomeRange)
+        //        Hit("AWESOME");
+        //    else if (_judgeValue < _goodRange)
+        //        Hit("GOOD");
+        //    else if (_judgeValue < _failRange)
+        //        Hit("FAIL");
+        //    else { }
+        //}
     }
 
     public void OnPointerExit(PointerEventData eventData)
---
+++ 2021_1_Project/Assets/Scripts/LongNoteStopover.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LongNoteStopover : MonoBehaviour
+{
+    private Image _image;
+
+    [Header("다음 경유지 노트")]
+    [SerializeField] private LongNoteStopover _nextStopover = default;
+
+    private bool _isSpread;
+
+    private float _fillAmount, _movedepartcircle;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+    private void Start()
+    {
+        _fillAmount = 1 / _image.rectTransform.sizeDelta.x;
+    }
+
+    private void Update()
+    {
+        if(_isSpread)
+        {
+            _image.fillAmount += _movedepartcircle * Time.deltaTime * _fillAmount;
+            if(_image.fillAmount == 1.0f)
+            {
+                if(_nextStopover != null)
+                    _nextStopover.SpreadNote(_movedepartcircle);
+                _isSpread = false;
+                _image.fillOrigin = 0; // LEFT 
+            }
+        }
+    }
+
+    public void SpreadNote(float _movedepartcircle)
+    {
+        this._movedepartcircle = _movedepartcircle;
+        _isSpread = true;
+    }
+
+    public float SetFillAmount(Vector3 _departNotePos)
+    {
+        _image.fillAmount = Mathf.Abs(_departNotePos.x - transform.position.x) * _fillAmount;
+        return _image.fillAmount;
+    }
+
+    public void SetFillAmount(float _value)
+    {
+        _image.fillAmount = _value;
+    }
+
+    public void ResetFillOrigin()
+    {
+        _image.fillOrigin = 1;
+    }
+}
