7727dec398d3a4fd8aa8661f7b06d7553f2dc4cf
DO <dogoo3@naver.com>
2021-05-24 12:00:51 +0900

    Monster에 대한 구조 일부 변경(선택하면 사라지는 형식이 아닌 두 몬스터 모두 살리는 형식)

--- 2021_1_Project/Assets/Scripts/Manager/MonsterManager.cs
+++ 2021_1_Project/Assets/Scripts/Manager/MonsterManager.cs
@@ -8,12 +8,15 @@ public class MonsterManager : MonoBehaviour
     public static MonsterManager instance;
 
     [SerializeField] private SelectMonster[] _monsters = default;
-    [SerializeField] private Image _gauge = default;
+    [SerializeField] private Image _redM_gauge = default;
+    [SerializeField] private Image _blueM_gauge = default;
     [SerializeField] private Image _curtain = default;
     [SerializeField] private Color _failColor = default;
     [SerializeField] private Color _successColor = default;
 
     private Dictionary<string, float> _gaugePoint = new Dictionary<string, float>();
+
+    private string _monsterType;
 
     private void Awake()
     {
@@ -26,7 +29,7 @@ public class MonsterManager : MonoBehaviour
 
     public void ChoiceMonster(string _type)
     {
-        if(_type == "_A") // Choice Red
+        if (_type == "_A") // Choice Red
         {
             _monsters[1].NonSelect();
         }
@@ -34,30 +37,55 @@ public class MonsterManager : MonoBehaviour
         {
             _monsters[0].NonSelect();
         }
+        _monsterType = _type;
     }
 
     public void StartMusic()
     {
+        if (_monsterType == "_A")
+        {
+            ActiveGauge(_redM_gauge);
+        }
+        else
+        {
+            ActiveGauge(_blueM_gauge);
+        }
+        _curtain.gameObject.SetActive(false);
+    }
+
+    private void ActiveGauge(Image _gauge)
+    {
         _gauge.color = _failColor;
         _gauge.gameObject.SetActive(true);
-        _curtain.gameObject.SetActive(false);
     }
 
     public void CarculateGauge(string _judge)
     {
         if (_gaugePoint.ContainsKey(_judge))
         {
-            _gauge.fillAmount = Mathf.Clamp(_gauge.fillAmount + _gaugePoint[_judge], 0, 1.0f);
-            if (_gauge.fillAmount >= 0.6f)
-                _gauge.color = _successColor;
+            if(_monsterType == "_A")
+            {
+                SetGauge(_redM_gauge, _judge);
+            }
             else
-                _gauge.color = _failColor;
+            {
+                SetGauge(_blueM_gauge, _judge);
+            }
         }
+    }
+
+    private void SetGauge(Image _gauge, string _judge)
+    {
+        _gauge.fillAmount = Mathf.Clamp(_gauge.fillAmount + _gaugePoint[_judge], 0, 1.0f);
+        if (_gauge.fillAmount >= 0.6f)
+            _gauge.color = _successColor;
+        else
+            _gauge.color = _failColor;
     }
 
     public void Init()
     {
-        _gauge.fillAmount = 0f;
-        _gauge.color = _failColor;
+        _redM_gauge.fillAmount = _blueM_gauge.fillAmount = 0f;
+        _redM_gauge.color = _blueM_gauge.color = _failColor;
     }
 }
--- 2021_1_Project/Assets/Scripts/Monster/SelectMonster.cs
+++ 2021_1_Project/Assets/Scripts/Monster/SelectMonster.cs
@@ -7,14 +7,9 @@ using UnityEngine.EventSystems;
 public class SelectMonster : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private string _monsterType = default;
-    [SerializeField] private Transform _arrivePos = default;
 
     private Image _image;
-    private Color _color = Color.white;
 
-    private Vector2 _departPos;
-
-    private bool _isSelect, _isNonselect;
     private float _lerpValue;
 
     private void Awake()
@@ -22,47 +17,18 @@ public class SelectMonster : MonoBehaviour, IPointerClickHandler
         _image = GetComponent<Image>();
     }
 
-    private void Start()
-    {
-        _departPos = transform.position;
-    }
-
     public void OnPointerClick(PointerEventData eventData)
     {
+        PlayMusicInfo.AppendMusicInfo(_monsterType);
+        SetNote.instance.ReadNoteFile();
+        NotePoolingManager.instance.ReadNoteFile();
         MonsterManager.instance.ChoiceMonster(_monsterType);
+        MonsterManager.instance.StartMusic();
         _image.raycastTarget = false;
-        _isSelect = true;
-    }
-
-    private void Update()
-    {
-        if(_isSelect) // 몬스터가 선택되었을 때
-        {
-            _color.a -= 0.04f;
-            _image.color = _color;
-            if (_image.color.a <= 0f)
-            {
-                PlayMusicInfo.AppendMusicInfo(_monsterType);
-                SetNote.instance.ReadNoteFile();
-                NotePoolingManager.instance.ReadNoteFile();
-                MonsterManager.instance.StartMusic();
-                _isSelect = false;
-            }
-        }
-        if(_isNonselect) // 몬스터가 선택되지 않았을 때
-        {
-            _lerpValue += 0.04f;
-            transform.position = Vector2.Lerp(_departPos, _arrivePos.position, _lerpValue); 
-            if(_lerpValue >= 1.0f)
-            {
-                _isNonselect = false;
-            }
-        }
     }
 
     public void NonSelect()
     {
         _image.raycastTarget = false;
-        _isNonselect = true;
     }
 }
